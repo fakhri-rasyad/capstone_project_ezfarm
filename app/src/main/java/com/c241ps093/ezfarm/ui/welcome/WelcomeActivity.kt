44a214ea9400002927a78a21b56d32eb3c4d079d
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-25 10:21:04 +0800

    feat: onboarding page and animation

--- app/src/main/java/com/c241ps093/ezfarm/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/c241ps093/ezfarm/ui/welcome/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.c241ps093.ezfarm.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.c241ps093.ezfarm.R
 import com.c241ps093.ezfarm.databinding.ActivityWelcomeBinding
+import com.c241ps093.ezfarm.ui.onboarding.OnboardingActivity
 
 class WelcomeActivity : AppCompatActivity() {
     private var _binding : ActivityWelcomeBinding? = null
@@ -15,6 +17,11 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.welcomeButton.setOnClickListener {
+            val intent = Intent(this, OnboardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     override fun onDestroy() {
