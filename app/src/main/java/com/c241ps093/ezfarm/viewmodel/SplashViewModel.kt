6e9cb2f48c3f1e83a585c9835cbc5b2dbedf439d
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-25 19:37:17 +0800

    feat: navigate to main when user has used app

--- app/src/main/java/com/c241ps093/ezfarm/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/c241ps093/ezfarm/viewmodel/SplashViewModel.kt
@@ -1,13 +1,15 @@
 package com.c241ps093.ezfarm.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.c241ps093.ezfarm.data.repository.EzFarmRepository
+import kotlinx.coroutines.runBlocking
 
-class SplashViewModel(private val ezFarmRepository: EzFarmRepository) {
+class SplashViewModel(private val ezFarmRepository: EzFarmRepository) : ViewModel() {
     suspend fun setHasUsedApp(){
         ezFarmRepository.setHasUsedApp()
     }
 
-    suspend fun checkHasUsedApp() : Boolean{
-        return ezFarmRepository.checkIfHasUsedApp()
+    fun checkHasUsedApp() : Boolean{
+        return runBlocking {ezFarmRepository.checkIfHasUsedApp()}
     }
 }
