5a6cd28387a4a1f6534679ee14a392b2a2e5a8a9
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-30 17:06:11 +0800

    feat: adding image taking feature

--- app/src/main/java/com/c241ps093/ezfarm/Utils.kt
+++ app/src/main/java/com/c241ps093/ezfarm/Utils.kt
@@ -2,6 +2,7 @@ package com.c241ps093.ezfarm
 
 import android.content.Context
 import android.widget.Toast
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -13,4 +14,9 @@ fun makeToast(context: Context, message: String){
 fun dateFormatter(date: Date): String {
     val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
     return dateFormat.format(date)
+}
+
+fun createCustomTempFile(context: Context): File {
+    val filesDir = context.externalCacheDir
+    return File.createTempFile("today", ".jpg", filesDir)
 }
--- app/src/main/java/com/c241ps093/ezfarm/ui/home/HomeActivity.kt
+++ app/src/main/java/com/c241ps093/ezfarm/ui/home/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.c241ps093.ezfarm.ui.home
 
-import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
